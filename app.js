1e7a38576129fd33258a19d414e5a69bfd62bf96
pjo901018 <pjo901018@korea.ac.kr>
2017-08-22 20:15:58 +0900

    params까지

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ var port = process.env.PORT || 3000;
 
 
 app.use(function(req, res, next) {
+  req.myMessage = "Hello, middleware #2";
   console.log("First piece of middleware");
   next();
 });
 
-app.use(function(req, res, next) {
-  console.log("Second piece of middleware");
+app.use("/different/:id", function(req, res, next) {
+  console.log("req.myMessage", req.myMessage);
+  console.log("Second piece of middleware, ID:", req.params.id);
   next();
 });
 
