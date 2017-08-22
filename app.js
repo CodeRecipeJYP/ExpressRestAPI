f6f6aa7dd2896d6cbd78111cad4e74591bafc1ff
pjo901018 <pjo901018@korea.ac.kr>
2017-08-22 23:21:28 +0900

    주석 오타 수정 catch 404 'adn' forward to error handler -> and

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(jsonParser());
 
 app.use("/questions", routes);
 
-// catch 404 adn forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
