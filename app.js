60bde8f747f521e93b42b9aabb51a292b7c7c1ad
pjo901018 <pjo901018@korea.ac.kr>
2017-08-22 19:08:14 +0900

    Adding Middleware

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var app = express();
 // console.log("process.env.PORT", process.env.PORT);
 var port = process.env.PORT || 3000;
 
+
+app.use(function(req, res, next) {
+  console.log("First piece of middleware");
+  next();
+});
+
+app.use(function(req, res, next) {
+  console.log("Second piece of middleware");
+  next();
+});
+
 app.listen(port, function() {
   console.log("express server is listening on port", port);
 });
