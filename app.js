6af7204a46cf020bbbc2cd6dd539fca3e432ba24
pjo901018 <pjo901018@korea.ac.kr>
2017-08-22 21:35:22 +0900

    middleware에 body-parser추가

--- app.js
+++ app.js
@@ -5,23 +5,23 @@
 
 var express = require("express");
 var app = express();
+var jsonParser = require("body-parser").json;
+
+var jsonCheck = function(req, res, next) {
+  if (req.body) {
+    console.log("The sky is", req.body.color);
+  } else {
+    console.log("There is no body property on the request");
+  }
+  next();
+};
 
 // console.log("process.env.PORT", process.env.PORT);
 var port = process.env.PORT || 3000;
 
-
-app.use(function(req, res, next) {
-  console.log("The leave on the trees are", req.query.color);
-  req.myMessage = "Hello, middleware #2";
-  console.log("First piece of middleware");
-  next();
-});
-
-app.use("/different/:id", function(req, res, next) {
-  console.log("req.myMessage", req.myMessage);
-  console.log("Second piece of middleware, ID:", req.params.id);
-  next();
-});
+app.use(jsonCheck);
+app.use(jsonParser());
+app.use(jsonCheck);
 
 app.listen(port, function() {
   console.log("express server is listening on port", port);
