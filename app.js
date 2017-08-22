388e63e97863dcb6b71d765d35dcc5739353bb8d
pjo901018 <pjo901018@korea.ac.kr>
2017-08-22 18:59:14 +0900

    Run server on 3000 port

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 /**
  * Created by jaeyoung on 8/22/17.
  */
+'use strict';
+
+var express = require("express");
+var app = express();
+
+// console.log("process.env.PORT", process.env.PORT);
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log("express server is listening on port", port);
+});
