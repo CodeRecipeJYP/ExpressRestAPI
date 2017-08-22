f1e1c217a95e433aa3b661dc66724c0500a71d1d
pjo901018 <pjo901018@korea.ac.kr>
2017-08-23 00:23:23 +0900

    Schema에 default값 추가 / Animal(Model).remove({})로 기존 Collection 비움

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -24,30 +24,46 @@ db.once("open", function() {
 
   var Schema = mongoose.Schema;
   var AnimalSchema = new Schema({
-    type: String,
-    size: String,
-    color: String,
-    mass: Number,
-    name: String
+    type:  {type: String, default: "goldfish"},
+    size:  {type: String, default: "small"},
+    color: {type: String, default: "golden"},
+    mass:  {type: Number, default: 0.007},
+    name:  {type: String, default: "Angela"}
   });
 
   var Animal = mongoose.model("Animal", AnimalSchema);
 
   var elephant = new Animal({
-    type: "elephant",
-    size: "big",
+    type:  "elephant",
+    size:  "big",
     color: "gray",
-    mass: 6000,
-    name: "Lawrence"
+    mass:  6000,
+    name:  "Lawrence"
   });
 
-  // It will fail because "save" is asynchronise function
-  elephant.save(function (err) {
-    if (err) console.error("Save Failed.", err);
-    else console.log("Saved!");
+  var animal = new Animal({}); // Goldfish
 
-    db.close(function () {
-      console.log("db connection closed");
+  var whale = new Animal({
+    type:  "whale",
+    size:  "big",
+    mass:  190500,
+    name:  "Fig"
+  });
+
+  // remove All documents
+  Animal.remove({}, function() {
+    // It will fail because "save" is asynchronise function
+    elephant.save(function (err) {
+      if (err) console.error("Save Failed.", err);
+      animal.save(function (err) {
+        if (err) console.error("Save Failed.", err);
+        whale.save(function (err) {
+          if (err) console.error("Save Failed.", err);
+          db.close(function () {
+            console.log("db connection closed");
+          });
+        });
+      });
     });
   });
 });
