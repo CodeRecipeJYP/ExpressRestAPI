e7d1037cbc1e748b31753f211fe8bcb76bf64c87
pjo901018 <pjo901018@korea.ac.kr>
2017-08-22 21:36:29 +0900

    jsonCheck 제거

--- app.js
+++ app.js
@@ -7,21 +7,10 @@ var express = require("express");
 var app = express();
 var jsonParser = require("body-parser").json;
 
-var jsonCheck = function(req, res, next) {
-  if (req.body) {
-    console.log("The sky is", req.body.color);
-  } else {
-    console.log("There is no body property on the request");
-  }
-  next();
-};
-
 // console.log("process.env.PORT", process.env.PORT);
 var port = process.env.PORT || 3000;
 
-app.use(jsonCheck);
 app.use(jsonParser());
-app.use(jsonCheck);
 
 app.listen(port, function() {
   console.log("express server is listening on port", port);
