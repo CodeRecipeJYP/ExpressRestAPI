5cdc58897d1a32b3e145d210423ae510616f2ab0
pjo901018 <pjo901018@korea.ac.kr>
2017-08-23 15:36:55 +0900

    Animal.create(animalData) 배열로 한꺼번에 인스턴스 추가함

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -24,28 +24,28 @@ db.once("open", function() {
 
   var Schema = mongoose.Schema;
   var AnimalSchema = new Schema({
-    type:  {type: String, default: "goldfish"},
-    size:  String,
+    type: {type: String, default: "goldfish"},
+    size: String,
     color: {type: String, default: "golden"},
-    mass:  {type: Number, default: 0.007},
-    name:  {type: String, default: "Angela"}
+    mass: {type: Number, default: 0.007},
+    name: {type: String, default: "Angela"}
   });
 
   var Animal = mongoose.model("Animal", AnimalSchema);
 
   var elephant = new Animal({
-    type:  "elephant",
+    type: "elephant",
     color: "gray",
-    mass:  6000,
-    name:  "Lawrence"
+    mass: 6000,
+    name: "Lawrence"
   });
 
   var animal = new Animal({}); // Goldfish
 
   var whale = new Animal({
-    type:  "whale",
-    mass:  190500,
-    name:  "Fig"
+    type: "whale",
+    mass: 190500,
+    name: "Fig"
   });
 
   var animalData = [
@@ -76,21 +76,16 @@ db.once("open", function() {
   // remove All documents
   Animal.remove({}, function () {
     // It will fail because "save" is asynchronise function
-    elephant.save(function (err) {
+    Animal.create(animalData, function (err) {
       if (err) console.error("Save Failed.", err);
-      animal.save(function (err) {
-        if (err) console.error("Save Failed.", err);
-        whale.save(function (err) {
-          if (err) console.error("Save Failed.", err);
-          Animal.find({size: "big"}, function (err, animals) {
-            animals.forEach(function (animal) {
-              console.log(animal.name + " the " + animal.color + " " + animal.type);
-            });
 
-            db.close(function () {
-              console.log("db connection closed");
-            });
-          });
+      Animal.find({size: "big"}, function (err, animals) {
+        animals.forEach(function (animal) {
+          console.log(animal.name + " the " + animal.color + " " + animal.type);
+        });
+
+        db.close(function () {
+          console.log("db connection closed");
         });
       });
     });
