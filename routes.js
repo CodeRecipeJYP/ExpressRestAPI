11c0ad18306582bd367121c180ff92b19b2b27b7
pjo901018 <pjo901018@korea.ac.kr>
2017-08-23 20:32:43 +0900

    REST Api 완성

--- routes.js
+++ routes.js
@@ -21,6 +21,16 @@ router.param("qId", function (req, res, next, id) {
   })
 });
 
+router.param("aId", function (req, res, next, id) {
+  req.answer = req.question.answer.id(id);
+  if (!req.answer) {
+    err = new Error("Not Found");
+    err.status = 404;
+    return next(err);
+  }
+  next();
+});
+
 // GET /questions
 // Return all the questions
 router.get("/", function(req, res) {
@@ -78,22 +88,32 @@ router.post("/:qId/answers", function(req, res, next) {
 // PUT /questions/:qId/answers/:aId
 // Edit a specific answer
 router.put("/:qId/answers/:aId", function(req, res) {
-  res.json({
-    response: "You sent me a PUT request to /answers",
-    questionId: req.params.qId,
-    answerId: req.params.aId,
-    body: req.body
+  req.answer.update(req.body, function (err, result) {
+    if (err) return next(err);
+    res.json(result);
   });
+  // res.json({
+  //   response: "You sent me a PUT request to /answers",
+  //   questionId: req.params.qId,
+  //   answerId: req.params.aId,
+  //   body: req.body
+  // });
 });
 
 // DELETE /questions/:qId/answers/:aId
 // Delete a specific answer
 router.delete("/:qId/answers/:aId", function(req, res) {
-  res.json({
-    response: "You sent me a DELETE request to /answers",
-    questionId: req.params.qId,
-    answerId: req.params.aId
+  req.answer.remove(function (err) {
+    req.question.save(function (err, question) {
+      if (err) return next(err);
+      res.json(question);
+    })
   });
+  // res.json({
+  //   response: "You sent me a DELETE request to /answers",
+  //   questionId: req.params.qId,
+  //   answerId: req.params.aId
+  // });
 });
 
 // POST /questions/:qId/answers/:aId/vote-up
@@ -106,16 +126,21 @@ router.post("/:qId/answers/:aId/vote-:dir",
       err.status = 404;
       next(err);
     } else {
+      req.vote = req.params.dir;
       next();
     }
   },
   function(req, res) {
-    res.json({
-      response: "You sent me a POST request to /vote-" + req.params.dir,
-      questionId: req.params.qId,
-      answerId: req.params.aId,
-      vote: req.params.dir
+    req.answer.vote(req.vote, function (err, question) {
+      if (err) return next(err);
+      res.json(question);
     });
+    // res.json({
+    //   response: "You sent me a POST request to /vote-" + req.params.dir,
+    //   questionId: req.params.qId,
+    //   answerId: req.params.aId,
+    //   vote: req.params.dir
+    // });
   });
 
 module.exports = router;
